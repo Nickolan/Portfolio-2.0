31e60aa525e28584a732c47db25147b860bdf160
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-08-24 18:45:46 -0300

    Navbar, Lenguage toogle button

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import './navbar.css'
+import { LeneguageContext } from '../LenguageContext'
 
 export default function Navbar() {
+
+  const {isEnglish, toogleLenguage} = useContext(LeneguageContext)
+  const [menuOpen, setMenuOpen] = useState(true)
+  const openBrugerNavbar = () =>{
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <div id='navbar'>
+    <nav id='navbar'>
       <div className='nav-container'>
         <div className='burger-link-container'>
-          <button>
-            <div></div>
-            <div></div>
-            <div></div>
+          <button className={!menuOpen ? 'btn-active' : 'btn'} onClick={openBrugerNavbar}>
+            <div id='div-01'></div>
+            <div id='div-02'></div>
+            <div id='div-03'></div>
           </button>
-          <div className='li-container'>
+          <div className={!menuOpen ? 'li-container-active' : 'li-container'}>
+            <h2 onClick={toogleLenguage}>{isEnglish ? 'En' : 'Es'}</h2>
             <ul>
               <li>
-                <a>Inicio</a>
+                <a>{isEnglish ? 'Home' : 'Inicio'}</a>
               </li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
+              <li>
+                <a>{isEnglish ? 'About Me' : 'Sobre Mi'}</a>
+              </li>
+              <li>
+                <a>{isEnglish ? 'Skills' : 'Habilidades'}</a>
+              </li>
+              <li>
+                <a>{isEnglish ? 'Projects' : 'Proyectos'}</a>
+              </li>
+              <li>
+                <a>{isEnglish ? 'Certificates' : 'Certificados'}</a>
+              </li>
+              <li>
+                <a>{isEnglish ? 'Contact Me' : 'Contactame'}</a>
+              </li>
             </ul>
           </div>
         </div>
       </div>
-    </div>
+    </nav>
   )
 }
