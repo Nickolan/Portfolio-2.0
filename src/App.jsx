be239425c5e8790561ec471bd6c816128e3b9693
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-09-30 15:37:11 -0300

    Mobile design complete

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,15 @@ import Contact from './components/Contact/Contact';
 function App() {
   const {thisColor} = useContext(LeneguageContext);
   return (
-        <div className='contentApp ' >
-            <div className='' style={{"--primary-color": thisColor ? "#4fc3dc" : "#ff2d75", "--secundary-color": thisColor ? "#234ebb" : "#c32739"}}>
-              <BgAnimated/>
-              <Navbar/>
-              <Home/>
-              <About/>
-              <Skills/>
-              <Projects/>
-              <Contact/>
-            </div>
-        </div>
+      <div className='' style={{"--primary-color": thisColor ? "#4fc3dc" : "#ff2d75", "--secundary-color": thisColor ? "#234ebb" : "#c32739"}}>
+          <BgAnimated/>
+          <Navbar/>
+          <Home/>
+          <About/>
+          <Skills/>
+          <Projects/>
+          <Contact/>
+      </div>
   )
 }
 
--- src/components/background animated/BgAnimated.jsx
+++ src/components/background animated/BgAnimated.jsx
@@ -3,22 +3,22 @@ import "./BgAnimated.css"
 
 const BgAnimated = () => {
   return (
-    <div className='container absolute -z-10 bottom-3'>
-      <div className='bubbles'>
-        <span style={{ "--i": 11 }}></span>
-        <span style={{ "--i": 15 }}></span>
-        <span style={{ "--i": 24 }}></span>
+      <div className='bubbles w-full'>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 13 }}></span>
+        <span style={{ "--i": 17 }}></span>
         <span style={{ "--i": 10 }}></span>
         <span style={{ "--i": 14 }}></span>
-        <span style={{ "--i": 23 }}></span>
-        <span style={{ "--i": 18 }}></span>
-        <span style={{ "--i": 30 }}></span>
         <span style={{ "--i": 19 }}></span>
-        <span style={{ "--i": 20 }}></span>
+        <span style={{ "--i": 13 }}></span>
+        <span style={{ "--i": 15 }}></span>
+        <span style={{ "--i": 18 }}></span>
         <span style={{ "--i": 22 }}></span>
-        <span style={{ "--i": 25 }}></span>
+        <span style={{ "--i": 13 }}></span>
+        <span style={{ "--i": 16 }}></span>
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 12 }}></span>
       </div>
-    </div>
   )
 }
 
